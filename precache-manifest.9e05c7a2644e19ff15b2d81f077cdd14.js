2909633d5ab4c1c811db2a3d8e5027058b5c2c24
Anatoly Ryumkin <rumsterrt@gmail.com>
2020-02-13 14:53:41 +0900

    Updates

---
+++ precache-manifest.9e05c7a2644e19ff15b2d81f077cdd14.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ab87cd6fa5dc1e55eeb0f66d91d79d08",
+    "url": "/geo-search-component/index.html"
+  },
+  {
+    "revision": "a8e6f2c5b99f8b92d898",
+    "url": "/geo-search-component/static/js/2.54b35c95.chunk.js"
+  },
+  {
+    "revision": "36c41eab3244f9ae8e4eff15670a4134",
+    "url": "/geo-search-component/static/js/2.54b35c95.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f93d30ed0aa76b6cecd7",
+    "url": "/geo-search-component/static/js/main.d7a86d63.chunk.js"
+  },
+  {
+    "revision": "9654643c021a5b84c936",
+    "url": "/geo-search-component/static/js/runtime-main.e325d294.js"
+  }
+]);
